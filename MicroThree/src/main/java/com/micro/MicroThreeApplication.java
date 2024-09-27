0ace1c7127fb5344be4b61f87e8a4698ed3b55ad
Kshitij <kshitijazad.ka@gmail.com>
2024-09-27 13:08:17 +0530

    microservices_nginx

---
+++ MicroThree/src/main/java/com/micro/MicroThreeApplication.java
@@ -0,0 +1,13 @@
+package com.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroThreeApplication.class, args);
+	}
+
+}
